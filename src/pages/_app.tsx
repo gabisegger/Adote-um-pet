9aa1879b44347909f72de5f2e6d25c696167e8df
gabisegger <gabi.segger@gmail.com>
2022-06-02 09:21:57 -0300

    feat: header creation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import '../ui/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import tema from '../ui/themes/tema';
+import Cabecalho from '../ui/components/Cabecalho/Cabecalho';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ThemeProvider theme={tema}>
+    <Cabecalho />
     <Component {...pageProps} />
     </ThemeProvider>
 }
