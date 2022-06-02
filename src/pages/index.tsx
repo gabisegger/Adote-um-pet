146609c2087cc705976ce80c0fe350ce9674aca7
gabisegger <gabi.segger@gmail.com>
2022-06-02 11:16:10 -0300

    feat: creation and styling of the list of pets for adoption

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,35 @@
 import type { NextPage } from 'next'
-import Titulo from '../ui/components/Titulo/Titulo';
-
+import Titulo from '../ui/components/Titulo/Titulo'
+import Lista from '../ui/components/Lista/Lista'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Titulo 
-      titulo="" 
-      subtitulo={
-      <span> 
-        Com um pequeno valor mensal, você <br />
-        pode <strong>adotar um pet virtualmente</strong>
-      </span>
+        titulo="" 
+        subtitulo={
+        <span> 
+          Com um pequeno valor mensal, você <br />
+          pode <strong>adotar um pet virtualmente</strong>
+        </span>
       } />
+
+      <Lista 
+        pets = {[
+          {
+            id: 1,
+            nome: 'Bidu',
+            historia: 'lorem ipsum dolor sit amet,sjkfkjdsvkshgrkn',
+            foto: 'https://st2.depositphotos.com/2222024/5609/i/600/depositphotos_56093859-stock-photo-happy-little-orange-havanese-puppy.jpg'
+          },
+          {
+            id: 2,
+            nome: 'Scooby',
+            historia: 'lorem ipsum dolor sit amet,sjkfkjdsvkshgrkn',
+            foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfJD1t5uQ3Hv4DBOlF4zBLfl_6OzPiW4R2vVVK9iBAmCXLoIrhFdSwkoTyihkmhebV9BU&usqp=CAU'
+          }
+        ]}
+      />
     </div>
   )
 }
