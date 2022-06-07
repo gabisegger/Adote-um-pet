c02e5470d5fa6cb7df0fdd231dc2262b214b52b4
gabisegger <gabi.segger@gmail.com>
2022-06-06 22:14:10 -0300

    feat: database connection

--- src/data/hooks/pages/useIndex.ts
+++ src/data/hooks/pages/useIndex.ts
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Pet } from '../../@types/Pet';
+import { ApiService } from '../../services/ApiService';
+import { AxiosError } from 'axios';
 
 export function useIndex(){
     const [listaPets, setListaPets] = useState<Pet[]>([]),
@@ -9,10 +11,42 @@ export function useIndex(){
         [mensagem, setMensagem] = useState('');
 
     useEffect(() => {
-        
+        ApiService.get('/pets')
+            .then((resposta) => {
+                setListaPets(resposta.data);
+            })
     }, [])
     
-    function adotar(){}
+    function adotar(){
+        if(petSelecionado !== null){
+            if(validarDadosAdocao()){
+                ApiService.post('/adocoes', {
+                    pet_id: petSelecionado.id,
+                    email,
+                    valor
+                })
+                    .then(() => {
+                        setPetSelecionado(null);
+                        setMensagem('Pet adotado com sucesso!');
+                        limparFormulario();
+                    })
+                    .catch((error: AxiosError) => {
+                        setMensagem(error.response?.data.errors.email);
+                    })
+            } else{
+                setMensagem('Preencha todos os campos corretamente!');
+            }
+        }
+    }
+
+    function validarDadosAdocao(){
+        return email.length > 0 && valor.length > 0;
+    }
+
+    function limparFormulario(){
+        setEmail('');
+        setValor('');
+    }
     
     return {
         listaPets,
