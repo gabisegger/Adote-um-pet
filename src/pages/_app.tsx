a0ba1fa998a8a5e13ecdb567f07357fabb5e9a02
gabisegger <gabi.segger@gmail.com>
2022-06-09 11:46:08 -0300

    feat: admin header creation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,18 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import tema from '../ui/themes/tema';
 import Cabecalho from '../ui/components/Cabecalho/Cabecalho';
+import CabecalhoAdmin from '../ui/components/CabecalhoAdmin/CabecalhoAdmin';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ThemeProvider theme={tema}>
-    <Cabecalho />
-    <Component {...pageProps} />
+  const router = useRouter();
+
+  return (
+    <ThemeProvider theme={tema}>
+      {router.pathname === '/' ? <Cabecalho/> : <CabecalhoAdmin/>}
+      <Component {...pageProps} />
     </ThemeProvider>
+  )
 }
 
 export default MyApp
